e04795cee7334ec90bc03f82c1b5320234afef22
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-01-10 11:59:01 +0100

    Move config with dts from results to root folder of case.

--- oscillator-overlap/convergence_study.sh
+++ oscillator-overlap/convergence_study.sh
@@ -3,7 +3,7 @@
 python3 doConvergenceStudy.py precice-config-template.xml -tss runge_kutta_4 generalized_alpha -wd 3 -dt 0.05 -sb 4 4 -s 8 -sf 1 2 -w 1 -o convergence-studies/subcycling_rk4_ga.csv
 python3 doConvergenceStudy.py precice-config-template.xml -tss runge_kutta_4 runge_kutta_4 -wd 3 -dt 0.05 -sb 4 4 -s 8 -sf 1 2 -w 1 -o convergence-studies/subcycling_rk4_rk4.csv
 # Figure 6.2a
-python3 doConvergenceStudy.py precice-config-template.xml --config convergence-studies/fig6_2a_config.csv -tss runge_kutta_4 generalized_alpha -wd 3 -o convergence-studies/contour_data.csv
+python3 doConvergenceStudy.py precice-config-template.xml --config fig6_2a_config.csv -tss runge_kutta_4 generalized_alpha -wd 3 -o convergence-studies/contour_data.csv
 # Figure 6.2b
 python3 doConvergenceStudy.py precice-config-template.xml -tss runge_kutta_4 generalized_alpha -dt 0.2 -wd 3 -sb 4 4 -o convergence-studies/compensation_S_2_4.csv
 python3 doConvergenceStudy.py precice-config-template.xml -tss runge_kutta_4 generalized_alpha -dt 0.2 -wd 3 -sb 4 8 -o convergence-studies/compensation_S_2_8.csv
