01f064c4689c153a0161af937de18f15a82943eb
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-01-18 16:13:01 +0100

    Provide configuration and scripts for Figure 6.6.
    * SC runs require preCICE 3.1.2 (substeps = false)
    * MC runs require preCICE c2e8989 or later (substeps = true)

--- perpendicular-flap/do_studies.sh
+++ perpendicular-flap/do_studies.sh
@@ -1,2 +1,2 @@
-python3 doConvergenceStudy.py precice-config-template.xml fig6_6_config.csv -o studies/fig6_6.csv
-python3 doConvergenceStudy.py precice-config-template.xml fig6_7_config.csv -o studies/fig6_7.csv
+python3 doConvergenceStudy.py precice-config-template-v3.1.2.xml fig6_6_config_precice_c2e8989.csv -o studies/fig6_6_precice_c2e8989.csv
+#python3 doConvergenceStudy.py precice-config-template.xml fig6_7_config.csv -o studies/fig6_7.csv  ## TODO
---
+++ perpendicular-flap/do_studies_v3.1.2.sh
@@ -0,0 +1,1 @@
+python3 doConvergenceStudy.py precice-config-template-v3.1.2.xml fig6_6_config_precice_v3.1.2.csv -o studies/fig6_6_precice_v3.1.2.csv
