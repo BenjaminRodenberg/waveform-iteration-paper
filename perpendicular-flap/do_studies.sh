d93e07d849e3c4cf03f037f11520d8d94d20791e
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-02-03 17:18:06 +0100

    Use larger window and simulation time for fig 6.7.

--- perpendicular-flap/do_studies.sh
+++ perpendicular-flap/do_studies.sh
@@ -1,2 +1,2 @@
 python3 doConvergenceStudy.py precice-config-template.xml fig6_6_config.csv -o studies/fig6_6.csv
-#python3 doConvergenceStudy.py precice-config-template.xml fig6_7_config.csv -o studies/fig6_7.csv  ## TODO
+python3 doConvergenceStudy.py precice-config-template.xml fig6_7_config.csv -o studies/fig6_7.csv
