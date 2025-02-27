aa05b02312b4d0d387a9cda18eed6bd02a17af01
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-02-27 16:49:00 +0100

    Split preprocessing and main process.
    * Allows to more conveniently run the OpenFOAM-deal.II/FEniCS.

---
+++ turek-hron-fsi3/prep_dealii.sh
@@ -0,0 +1,2 @@
+cp precice-config-dealii.xml precice-config.xml
+cp fluid-openfoam/system/preciceDict-dealii fluid-openfoam/system/preciceDict
---
+++ turek-hron-fsi3/prep_fenics.sh
@@ -0,0 +1,2 @@
+cp precice-config-fenics.xml precice-config.xml
+cp fluid-openfoam/system/preciceDict-fenics fluid-openfoam/system/preciceDict
