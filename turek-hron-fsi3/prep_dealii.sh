dcfa824881dc0c87de8bb747385d44f0fbab4f08
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2025-07-15 22:02:35 +0200

    Cleanup.

--- turek-hron-fsi3/prep_dealii.sh
+++
@@ -1,2 +0,0 @@
-cp precice-config-dealii.xml precice-config.xml
-cp fluid-openfoam/system/preciceDict-dealii fluid-openfoam/system/preciceDict
--- turek-hron-fsi3/prep_fenics.sh
+++
@@ -1,2 +0,0 @@
-cp precice-config-fenics.xml precice-config.xml
-cp fluid-openfoam/system/preciceDict-fenics fluid-openfoam/system/preciceDict
