3028b1c82cb9e1c78c3fd2c480460dde70b23c52
Benjamin Rodenberg <rodenberg@dkrz.de>
2025-10-12 16:51:15 +0200

    Fix path.

--- experiments/oscillator-overlap/run_experiments.sh
+++ experiments/oscillator-overlap/run_experiments.sh
@@ -17,4 +17,4 @@ python3 doConvergenceStudy.py precice-config-FP.xml.jinja2 --silent --executor G
 ## Table 1 optimal relationship of dts
 python3 doConvergenceStudy.py precice-config-FP.xml.jinja2 --config configs/tab1_dts.csv -tss runge_kutta_4 generalized_alpha -wd 3 --exchange-substeps -o results/Tab1/data/FP.csv
 python3 doConvergenceStudy.py precice-config-rQN.xml.jinja2 --config configs/tab1_dts.csv -tss runge_kutta_4 generalized_alpha -wd 3 --exchange-substeps -o results/Tab1/data/rQN.csv
-python3 doConvergenceStudy.py precice-config-fQN.xml.jinja2 --config configs/tab1_dts.csv -tss runge_kutta_4 generalized_alpha -wd 3 --exchange-substeps -o results/Tab1/data/rQN.csv
+python3 doConvergenceStudy.py precice-config-fQN.xml.jinja2 --config configs/tab1_dts.csv -tss runge_kutta_4 generalized_alpha -wd 3 --exchange-substeps -o results/Tab1/data/fQN.csv
